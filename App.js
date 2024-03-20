6ec6227b89cfe36ab65f485c42a23e86dbef6b23
Tomos <tswoolley@icloud.com>
2024-03-20 08:18:23 +0000

    Now in row direction.

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { appStyle } from "./App.style";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
+      <SafeAreaView style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
         <View style={appStyle.box1} />
         <View style={appStyle.box2} />
         <View style={appStyle.box3} />
