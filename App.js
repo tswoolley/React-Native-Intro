49038b9ea5c4aa6b7dcd260d655a0613843707f5
Tomos <tswoolley@icloud.com>
2024-03-27 09:44:36 +0000

    Added age counter, updates in terminal only.

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { ProfileCard } from "./components/ProfileCard/ProfileCard";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { appStyle } from "./App.style";
+import { AgeCounter } from "./components/AgeCounter/AgeCounter";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-        <ProfileCard
+        <AgeCounter/>
+        {/*<ProfileCard
           firstName={"Tomos"}
           lastName={"The Dev"}
           age={18}
           isOpenToWork
-        />
+  /> */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- components/AgeCounter/AgeCounter.jsx
+++ components/AgeCounter/AgeCounter.jsx
@@ -1,3 +1,15 @@
+import { TouchableOpacity, Text  } from "react-native";
+
 export function AgeCounter() {
-    return <></>;
+    let age = 30
+    function increaseAge() {
+        age++;
+        console.log(age)
+    }
+    return <>
+    <TouchableOpacity onPress={increaseAge} style={{backgroundColor: "#00A2FF"}}>
+        <Text style={{fontSize: 40, color: "white"}}>Increase age</Text>
+    </TouchableOpacity>
+    <Text style={{fontSize:40}}>I am {age} years old.</Text>
+    </>;
 }
