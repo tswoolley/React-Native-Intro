dc2643b8b392d5a1ac792118e46e6d2154d63403
Tomos <tswoolley@icloud.com>
2024-03-27 10:55:12 +0000

    Added new component.

--- components/AgeCounter/AgeCounter.jsx
+++ components/AgeCounter/AgeCounter.jsx
@@ -1,15 +1,21 @@
-import { TouchableOpacity, Text  } from "react-native";
+import { useState } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { DisplayAge } from "../DisplayAge/DisplayAge";
 
 export function AgeCounter() {
-    let age = 30
-    function increaseAge() {
-        age++;
-        console.log(age)
-    }
-    return <>
-    <TouchableOpacity onPress={increaseAge} style={{backgroundColor: "#00A2FF"}}>
-        <Text style={{fontSize: 40, color: "white"}}>Increase age</Text>
-    </TouchableOpacity>
-    <Text style={{fontSize:40}}>I am {age} years old.</Text>
-    </>;
+  const [age, setAge] = useState(30);
+  function increaseAge() {
+    setAge(age + 1);
+  }
+  return (
+    <>
+      <TouchableOpacity
+        onPress={increaseAge}
+        style={{ backgroundColor: "#00A2FF", borderRadius: 10, padding: 20 }}
+      >
+        <Text style={{ fontSize: 40, color: "white" }}>Increase age</Text>
+      </TouchableOpacity>
+      <DisplayAge age ={age}/>
+    </>
+  );
 }
