0de7d07003808c44b3d94a587764a70d585cefee
Tomos <tswoolley@icloud.com>
2024-03-27 13:45:17 +0000

    Using states.

--- components/AgeCounter/AgeCounter.jsx
+++ components/AgeCounter/AgeCounter.jsx
@@ -4,9 +4,16 @@ import { DisplayAge } from "../DisplayAge/DisplayAge";
 
 export function AgeCounter() {
   const [age, setAge] = useState(30);
+  const [fruit, setFruit] = useState("Banana")
   function increaseAge() {
     setAge(age + 1);
   }
+
+  function changeFruit(){
+    setFruit("Apple")
+  }
+
+
   return (
     <>
       <TouchableOpacity
@@ -16,6 +23,13 @@ export function AgeCounter() {
         <Text style={{ fontSize: 40, color: "white" }}>Increase age</Text>
       </TouchableOpacity>
       <DisplayAge age ={age}/>
+      <TouchableOpacity
+        onPress={changeFruit}
+        style={{ backgroundColor: "#00A2FF", borderRadius: 10, padding: 20 }}
+      >
+        <Text style={{ fontSize: 40, color: "white" }}>Change Fruit</Text>
+      </TouchableOpacity>
+      <Text>{fruit}</Text>
     </>
   );
 }
